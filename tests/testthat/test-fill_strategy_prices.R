fab160d1b156c537a1b41f65eeb38d17e52d2177
Christopher Sumter <csumter1165@gmail.com>
2019-10-10 19:47:38 +0100

    Fixed test

--- tests/testthat/test-fill_strategy_prices.R
+++ tests/testthat/test-fill_strategy_prices.R
@@ -1,9 +1,9 @@
 test_that("Refreshing prices returns correct price data", {
   hours_diff <- lubridate::hour(Sys.time()) - lubridate::hour(lubridate::with_tz(Sys.time(), "UTC"))
 
-  start_time <- format_price_request(strategy$legs[[1]]$prices$date_time[1])
+  start_time <- format_price_request(strategy$legs[[1]]$prices$date_time[1],0)
 
-  end_time <- format_price_request(tail(strategy$legs[[1]]$prices$date_time, 1))
+  end_time <- format_price_request(tail(strategy$legs[[1]]$prices$date_time, 1),0)
 
   strategy$legs[[1]]$prices <- head(strategy$legs[[1]]$prices, 10)
 
